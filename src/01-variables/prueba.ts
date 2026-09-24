cc9e8f3203a4ad079de09654b366bbbd510fb21f
DavidCode-Hub <davidsevalara@gmail.com>
2026-09-24 18:11:23 +0200

    Ampliación del documento de pruebas (Objetos, colores de caracteres, etc)

--- src/01-variables/prueba.ts
+++ src/01-variables/prueba.ts
@@ -1,8 +1,10 @@
+import { log } from "console";
+import { styleText } from "util";
+
 let nombre: string = "Jose";
-const hola2 = "Ruben";
+let nombre2: string = "Hola";
 
 console.log(nombre);
-console.log(`Hola roberto estamos aqui con ${hola2}`);
 
 function saludo(nombre: string) {
     console.log(`Hola ${nombre}`);
@@ -11,4 +13,25 @@ nombre = "david";
 
 saludo(nombre);
 
+function saludar(nombre2: string, nombre: string): string {
+    return `${nombre2} ${nombre}`
+}
 
+console.log("prueba de funcion saludar");
+
+let saludoecho = saludar(nombre2, nombre);
+
+console.log(saludoecho);
+
+console.log(styleText('bold', saludoecho));
+
+const coche = { matricula: "123456T", color: "verde", tipo:"coupe" };
+
+console.log(styleText('white',`Matricula: ${coche.matricula} y el coche es de tipo ${coche.tipo}`));
+
+console.log(styleText('red',`Matricula: ${coche.matricula} y el coche es de tipo ${coche.color}`));
+
+coche.matricula="3453245";
+coche.color="Blanco Perla";
+
+console.log(styleText('blue',`Matricula: ${coche.matricula} y el coche es de tipo ${coche.color}`));
