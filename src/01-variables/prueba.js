d42ead8f4a9e2fb4e6b8884a91934e92dc68afad
DavidCode-Hub <davidsevalara@gmail.com>
2026-09-24 14:58:44 +0200

    Creacion de fichero Pruebas de TypeScript

---
+++ src/01-variables/prueba.js
@@ -0,0 +1,10 @@
+var nombre = "Jose";
+var hola2 = "Ruben";
+console.log(nombre);
+console.log("Hola roberto estamos aqui con ".concat(hola2));
+function saludo(nombre) {
+    console.log("Hola ".concat(nombre));
+}
+saludo(nombre);
+
+
---
+++ src/01-variables/prueba.ts
@@ -0,0 +1,14 @@
+let nombre: string = "Jose";
+const hola2 = "Ruben";
+
+console.log(nombre);
+console.log(`Hola roberto estamos aqui con ${hola2}`);
+
+function saludo(nombre: string) {
+    console.log(`Hola ${nombre}`);
+}
+nombre = "david";
+
+saludo(nombre);
+
+
